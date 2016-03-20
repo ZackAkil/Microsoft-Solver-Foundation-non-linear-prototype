6f97d125e97693123dd04a07aa7575595f1cbb2f
ZackAkil <zackakil94@gmail.com>
2016-03-20 18:55:37 +0000

    added real data and data feed into cost function

--- SolverTest/Program.cs
+++ SolverTest/Program.cs
@@ -11,43 +11,81 @@ using MathNet.Numerics.LinearAlgebra.Solvers;
 using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Double;
 using MathNet.Numerics;
-
+using System.IO;
 
 namespace SolverTest
 {
     class Program
     {
 
-        public static double MyCostFunctionMethod(double[] theta)
+        public Matrix<Double> featureValues;
+        public Vector<Double> trueValues;
+
+        public static double[,] readFileIntoArray()
+        {
+            var items = File.ReadAllLines("testData") // read lines from file
+                    .Select(line => line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(Double.Parse).ToArray());
+
+            double[][] valuesFromFile = new double[100][];
+
+            int i = 0;
+            foreach (var item in items)
+            {
+                valuesFromFile[i] = item;
+                    i++;
+            }
+
+
+          //  Console.WriteLine(items);
+
+            return JaggedToMultidimensional(valuesFromFile);
+        }
+
+        public static double[] readResultFileIntoArray()
+        {
+            var items = File.ReadAllLines("saveResult") // read lines from file
+                    .Select(Double.Parse).ToArray();
+
+            double[] valuesFromFile = new double[100];
+
+            int i = 0;
+            foreach (var item in items)
+            {
+                valuesFromFile[i] = item;
+                i++;
+            }
+
+
+            //  Console.WriteLine(items);
+
+            return valuesFromFile;
+        }
+
+        public static T[,] JaggedToMultidimensional<T>(T[][] jaggedArray)
+        {
+            int rows = jaggedArray.Length;
+            int cols = jaggedArray.Max(subArray => subArray.Length);
+            T[,] array = new T[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    array[i, j] = jaggedArray[i][j];
+                }
+            }
+            return array;
+        }
+
+        public static double MyCostFunctionMethod(double[] theta,Matrix<Double> featureValues, Vector<Double> trueValues)
         {
             // Can add more complicated logic here
-        Matrix<Double> featureValues = DenseMatrix.OfArray(new double[,] {
-        {1,0.1},
-        {1,0.2},
-        {1,0.3},
-        {1,0.4},
-        {1,0.5},
-        {1,0.6},
-        {1,0.7},
-        {1,0.8},
-        {1,0.9},
-        {1,1},});
 
-            Vector<Double> trueValues = DenseVector.OfArray(new double[] {
-0.95166,
-   0.96953,
-   0.98092,
-   0.98811,
-   0.99261,
-   0.99541,
-   0.99716,
-   0.99824,
-   0.99891,
-   0.99932});
+
 
             Vector<Double> temptheta = DenseVector.OfArray(theta);
-            Console.WriteLine(theta[0].ToString() + " - " + theta[1].ToString());
-            Console.WriteLine(Distance.MSE(predictFunction(featureValues, temptheta), trueValues));
+         //   Console.WriteLine(theta[0].ToString() + " - " + theta[1].ToString());
+          //  Console.WriteLine(Distance.MSE(predictFunction(featureValues, temptheta), trueValues));
 
             return Distance.MSE(predictFunction(featureValues,temptheta), trueValues);
         }
@@ -62,7 +100,7 @@ namespace SolverTest
         {
             
             var result = (1 / (values.Multiply(-1).PointwiseExp() + 1));
-            Console.WriteLine(result);
+         //   Console.WriteLine(result);
             return result;
         }
 
@@ -70,21 +108,43 @@ namespace SolverTest
         static void Main(string[] args)
         {
 
-            double[] xInitial = new double[] { -1, 1 };
-            double[] xLower = new double[] { -10, -10 };
-            double[] xUpper = new double[] { 10, 10 };
 
-            Func < double[], double> costFunction = MyCostFunctionMethod;
-
-            var solution = NelderMeadSolver.Solve(costFunction, xInitial, xLower, xUpper);
+            readResultFileIntoArray();
             
+            
+
+             readFileIntoArray();
+
+            double[] xInitial = new double[17] { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 };
+            double[] xLower = new double[17] { -5,-5,-5,-5, -5, -5, -5, -5, -5, -5, -5, -5, -5, -5, -5, -5, -5 };
+            double[] xUpper = new double[17] { 5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5 };
+
+           // Func <double[], double> costFunction = MyCostFunctionMethod;
+
+            //var solution = NelderMeadSolver.Solve(costFunction, xInitial, xLower, xUpper);
+
+            Matrix<Double> featureValues = DenseMatrix.OfArray(readFileIntoArray());
+
+            Vector<Double> trueValues = DenseVector.OfArray(readResultFileIntoArray());
+
+            int y = 7;
+
+            var solution = NelderMeadSolver.Solve(x => MyCostFunctionMethod(x,featureValues,trueValues), xInitial, xLower, xUpper);
+
+
 
             Console.WriteLine(solution.Result);
             Console.WriteLine("solution = {0}", solution.GetSolutionValue(0));
-            Console.WriteLine("x = {0}", solution.GetValue(1));
-            Console.WriteLine("y = {0}", solution.GetValue(2));
+            for (int i = 0; i < 17; i++)
+            {
+                Console.WriteLine("x = {0}", solution.GetValue(i+1));
+            }
+
+            
 
             Console.ReadKey();
         }
+
+
     }
 }
